d1036e180f74286731159f1b7864707ca6225ecb
Vsevolod-pop <vopopenko@gmail.com>
2025-03-21 02:00:35 +0300

    Добавьте файлы проекта.

---
+++ LW4_T1/LW4_T1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string stroka;
+    int sum = 0;
+    char flag = '+';
+    int f = 1;
+    cout << "Введите строку:  ";
+    getline(cin, stroka);
+    while (f == 1)
+    {
+        f = 0;
+        if (stroka.length() % 2 == 0)
+        {
+            f = 1;
+            cout << "Вы ввели строку неверно, введите ее корректно по типу <цифра> +- <цифра> и тд. -  ";
+            getline(cin, stroka);
+            continue;
+        }
+        for (int i = 0; i < stroka.length(); i++)
+        {
+            cout << sum << endl;
+            if (i == 0 and isdigit(stroka[i]))
+            {
+                sum = stroka[i]-48;
+                continue;
+            }
+            if (i%2 != 0)
+            {
+                if (stroka[i] == '-')
+                {
+                    flag = '-';
+                    continue;
+                }
+                else if (stroka[i] == '+')
+                {
+                    flag = '+';
+                    continue;
+                }
+                else
+                {
+                    f = 1;
+                    cout << "В ввели строку неверно, введите ее корректно по типу <цифра> +- <цифра> и тд. -  ";
+                    getline(cin, stroka);
+                    break;
+                }
+            }
+            if (i % 2 == 0 and isdigit(stroka[i]) and flag == '-')
+            {
+                sum -= stroka[i]-48;
+            }
+            else if (i % 2 == 0 and isdigit(stroka[i]) and flag == '+')
+            {
+                sum += stroka[i]-48;
+            }
+            else
+            {
+                f = 1;
+                cout << "ВS ввели строку неверно, введите ее корректно по типу <цифра> +- <цифра> и тд. -  ";
+                getline(cin, stroka);
+                break;
+            }
+        }
+        if (f == 0)
+        {
+            break;
+        }
+    }
+    cout << stroka << endl << sum << endl;
+}
+
+
